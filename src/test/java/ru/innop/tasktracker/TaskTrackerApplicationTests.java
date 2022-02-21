d53e01d55c3b13226ab53d8590b688c6bac8a1a7
Antony-Sk <SkorkinAnton@yandex.ru>
2022-02-22 01:17:12 +0300

    Add docker

--- src/test/java/ru/innop/tasktracker/TaskTrackerApplicationTests.java
+++ src/test/java/ru/innop/tasktracker/TaskTrackerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskTrakerApplicationTests {
+class TaskTrackerApplicationTests {
 
     @Test
     void contextLoads() {
